ae7ee47c28991a7e377cb5e1c0712729a0738524
Arnold <arnoldchow@outlook.com>
2023-03-06 16:59:17 +0800

    update activities and layouts. TODO: complete SingleNight and MainActivity.

---
+++ app/src/main/java/com/arnold/sleepminder/activities/SingleNight.java
@@ -0,0 +1,12 @@
+package com.arnold.sleepminder.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+public class SingleNight extends AppCompatActivity {
+    public static String EXTRA_FILE = "file";
+
+    private File file;
+
+}
